dc02096a37c6343cf11314aeba220b7420885d4c
Brock <3rb@ornl.gov>
2021-12-07 15:45:30 -0600

    started displaying a list of heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Teenage Mutant Ninja Turtles' },
+  { id: 12, name: 'Daredevil' },
+  { id: 13, name: 'Flash Gordon' },
+  { id: 14, name: 'The Phantom' },
+  { id: 15, name: 'Rouge' },
+  { id: 16, name: 'Green Hornet' },
+  { id: 17, name: 'Goku' },
+  { id: 18, name: 'Vageta' },
+  { id: 19, name: 'Piccolo' },
+  { id: 20, name: 'Mater Roshi' }
+];
