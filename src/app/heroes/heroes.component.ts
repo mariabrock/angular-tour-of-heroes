ac7ec61ff69d548dcec0c8bd4d76442c78967685
Brock <3rb@ornl.gov>
2021-12-07 20:39:45 -0600

    finished styles on displayed list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,15 +8,18 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
   heroes = HEROES;
-  // hero: Hero = {
-  //   id: 1,
-  //   name: 'Storm'
-  // };
+  selectedHero?: Hero;
 
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
